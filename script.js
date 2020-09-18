7a5396ab8d156c06cf5e0385fe684a02bf06706f
Radiant <you@example.com>
2020-09-18 21:20:32 +0700

    menambah dua files

---
+++ script.js
@@ -0,0 +1,10 @@
+function mentorProgrammer(nama, status, asal) {
+    console.log('Nama Mentor', nama)
+    console.log(status)
+    console.log(asal)
+}
+
+function nama() {
+    console.log;
+}
+mentorProgrammer('Wandi Pratama', 'Status nya Santri', 'Berasal dari kota Prabulih')
